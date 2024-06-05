77afad1e27ae182460f874f7e96249c377ac3c66
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-06-05 12:38:36 +0300

    intro to python

---
+++ myscript.py
@@ -0,0 +1,67 @@
+print("we support Ghaza")
+
+#comment in python
+
+num = 10
+Num =20
+
+
+
+name = 'ahmed'
+
+email = 'ahmed@gmail.com'
+
+# if(name=='ahmed')
+# {print("hi")}
+
+# no braces no ; just indentation ?
+num = 10
+if name =='ahmed':
+    print("Hello Ahmed")
+    if num ==10:
+        print("found")
+
+# we use quotes to define the string --> single, double quotes the same
+
+bio = 'my name is noha'
+bio2= "I works at iti"
+
+#
+# bio = ("My name is Noha"
+#        "I works at iti"
+#        "I lives in cairo")
+#
+# print(bio)
+# multi-line string
+bio = ("My name is Noha \n"
+       "I works at iti \n"
+       "I lives in cairo")
+
+print(bio)
+
+# use triple quoted string
+
+mybio = '''My name is Noha
+I works at iti
+I lives in cairo
+I am teaching Python'''
+
+# use string as a comment
+
+name = 'noha'
+
+'this string acts like a comment'
+
+
+##############################
+happy = True
+g = 9.8
+
+# to display datatype of variable in runtime
+
+print(type(g))
+print(type(num))
+print(type(bio))
+
+
+
