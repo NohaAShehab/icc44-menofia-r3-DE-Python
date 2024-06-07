8ad8dd95b3a0566defeffbb63467c5ddeb8bff7d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-06-07 13:05:56 +0300

    functions

---
+++ sets.py
@@ -0,0 +1,46 @@
+
+print("we support ghaza")
+
+
+newstring = 'we have {stdnums} student in {crsname} course, {mm}, {ee}'
+
+print(newstring.format(stdnums='33',crsname='dfdf', mm=44, ee='ee'))
+
+
+"""
+    In Lab ---> IT is not allowed to use any syntax --> python function 
+    out the points discussed in the  past 3 lecture
+"""
+
+"""
+    lab will start now 
+    break from 12:45 to 1:45
+    |--------------|
+    | end of lab 4 |
+    |--------------|
+    
+    
+    'abdulrahman'
+    abdu  # longest ordered substring
+    lr
+    ahm
+    an
+        {apple , kiwi , banana, orange}
+        selected_word =mys.pop()
+        # get len of selected_word
+        
+        name ?  noha
+        ----
+        you have  7tries try to guess word ? 
+        enter char? s 
+        ----
+        enter char ? i 
+        -i-i
+        enter char ? k
+        ki-i
+        ---> you lost the game 
+        ?? ==>  completed the word ??---> You won 
+            
+            
+        
+"""
