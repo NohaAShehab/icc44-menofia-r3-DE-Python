2d9ef2461dbdadd81852a58ef3b90ba605941226
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-06-08 09:46:12 +0300

    scoping

---
+++ playground.py
@@ -0,0 +1,37 @@
+
+
+course = 'python'
+
+print("iti")
+
+
+print("----------------------------")
+
+print(course)
+
+del course
+
+print("-----data ----")
+
+#######################333
+def testfun():
+    # variable defined in the function #
+    name = input("What is your name?") # local variable
+    # can be accessed only inside a function and will be destroyed?
+    # once the function finished
+    print(name)
+
+testfun()
+
+
+
+
+
+
+
+
+
+
+
+
+
