f215efbc14181ec6f2c8d96b90d99660342a0cd3
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-06-11 09:54:46 +0300

    encapsulation

---
+++ playground.py
@@ -0,0 +1,28 @@
+class Employee:
+    def __init__(self, name, email, salary):
+        self.name = name  # public
+        self._email = email  # protected
+        self.salary = salary
+
+    def display(self):
+        print(f"name{self.name} , _email={self._email} , salary={self.__salary}")
+
+    @property
+    def netSalary(self):
+        return self.__salary * .8
+
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary):
+        print("--- setter called ----")
+        if isinstance(salary, int) or isinstance(salary, float) and salary > 0:
+            self.__salary = salary
+        else:
+            raise TypeError("salary must be an integer or float")
+
+
+emp = Employee("fff", "fff", 444)
+print("-----")
